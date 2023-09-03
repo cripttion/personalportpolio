2fa1dcf746c9853cea4f0eec3874961f057e59e8
cripttion <pulakshri@gmail.com>
2023-09-04 04:12:45 +0530

    Homesetion completed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,57 @@
 import React from 'react'
-
+import profileImg from '../assests/Images/profileImg.jpeg';
+import { TypeAnimation } from 'react-type-animation';
+import { AiFillLinkedin,AiFillGithub,AiFillTwitterCircle,AiFillInstagram,AiOutlineArrowDown } from 'react-icons/ai';
 function Home() {
   return (
-    <div>
+    
+    <div className='Hmain'>
         
+        <div className='flex justify-center mt-32'>
+            <div className='profileImage border-8 border-teal-300'>
+                <img  src={profileImg} alt='profile-Image'  className='self-center'/>
+               
+            </div>
+            
+        </div>
+    
+    
+        <div className='TypedElement text-teal-300 flex flex-row justify-center'>
+      
+        
+        <p className='text-white' style={{fontSize:'3rem'}}>Hi, I am <TypeAnimation
+                    sequence={[
+                    "Pulak Raj",
+                    1000,
+                    "Cirpttion",
+                    1000,
+                    "Full-stack Developer",
+                    1000,
+                    "React-Native Developer",
+                    1000,
+                    ]}
+                    speed={20}
+                    repeat={Infinity}
+                    cursor={true}
+                    style={{ fontSize: '3rem',color:'rgb(94 234 212)'}}
+                /></p>
+                
+  </div>
+  <div className='Habout text-white flex justify-center'>
+    <div className='w-1/2 mt-5'><p className='text-cener'>I'm a versatile full-stack developer with a strong passion for crafting robust and user-centric web and mobile solutions. With expertise in front-end and back-end technologies, as well as React Native, I thrive on turning ideas into elegant, functional, and scalable digital experiences. Let's collaborate to bring your vision to life!</p>
     </div>
+  </div>
+  <div className='Links flex flex-row justify-center mt-20 text-4xl text-white gap-10'>
+    <div className='LinkIcons border border-slate-600 p-3 rounded-xl'><a href='https://www.linkedin.com/in/pulak-cripttion/'><AiFillLinkedin /></a></div>
+    <div  className='LinkIcons border border-slate-600 p-3 rounded-xl'><AiFillGithub /></div>
+    <div className='LinkIcons border border-slate-600 p-3 rounded-xl'><AiFillTwitterCircle /></div>
+    <div className='LinkIcons border border-slate-600  p-3 rounded-xl'><AiFillInstagram /></div>
+  </div>
+
+  <div className='ScrollDown flex justify-center text-slate-600 mt-56'>
+        <span className='flex text-xl'><AiOutlineArrowDown className='border border-slate-600 rounded-full mr-2' /></span> <span>Scroll</span>
+  </div>
+  </div>
   )
 }
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>
-        <header>
+    <div className='border-b'>
+        <header style={{backgroundColor:'#13142E'}}>
   <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
     <div className="flex lg:flex-1">
     
